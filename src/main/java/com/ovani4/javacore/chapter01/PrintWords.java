51339c0f5679013cc1608b8d138adb08000bfdc7
Ovani4 <ovani4@ya.ru>
2021-01-18 11:19:54 +0300

    chapter 01 add Class PrintWords

---
+++ src/main/java/com/ovani4/javacore/chapter01/PrintWords.java
@@ -0,0 +1,7 @@
+package main.java.com.ovani4.javacore.chapter01;
+
+public class PrintWords {
+    public static void main(String[] args) {
+        System.out.println("В первой главе описана история Java и она не компилируется)))");
+    }
+}
