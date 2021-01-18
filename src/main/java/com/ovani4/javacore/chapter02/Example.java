a576bb27472d0469e5db105f9d53f0346a488e25
Ovani4 <ovani4@ya.ru>
2021-01-18 11:23:55 +0300

    crate package02 and Class Example

---
+++ src/main/java/com/ovani4/javacore/chapter02/Example.java
@@ -0,0 +1,7 @@
+package main.java.com.ovani4.javacore.chapter02;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Первая программа на Java.");
+    }
+}
