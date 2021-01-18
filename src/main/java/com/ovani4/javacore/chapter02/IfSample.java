b49f2228a44257ddc315dbf77b7c5e505eb4bb4e
Ovani4 <ovani4@ya.ru>
2021-01-18 11:37:06 +0300

    crate Class IfSample

---
+++ src/main/java/com/ovani4/javacore/chapter02/IfSample.java
@@ -0,0 +1,19 @@
+package main.java.com.ovani4.javacore.chapter02;
+
+public class IfSample {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+
+        if (x < y) System.out.println("х меньше у");
+
+        x = x * 2;
+        if (x == y) System.out.println("x теперь равно y");
+
+        x = x * 2;
+        if (x > y)System.out.println("x теперь больше y");
+
+        if (x == y) System.out.println("Это не будет выводится");
+    }
+}
