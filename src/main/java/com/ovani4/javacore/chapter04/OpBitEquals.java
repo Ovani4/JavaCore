fbf0484173e765ff7824900fcf9374e667f8e4b0
Ovani4 <ovani4@ya.ru>
2021-01-29 14:13:21 +0000

    crate Class OpBitEquals

---
+++ src/main/java/com/ovani4/javacore/chapter04/OpBitEquals.java
@@ -0,0 +1,19 @@
+package main.java.com.ovani4.javacore.chapter04;
+
+public class OpBitEquals {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        a |= 4;
+        System.out.println("a = " + a);
+        b >>=1;
+        c <<= 1;
+        a ^= c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
