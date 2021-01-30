61a457121ef34063b8e0773f43ac4093e4f28a16
Ovani4 <ovani4@ya.ru>
2021-01-30 18:56:05 +0000

    crate Class Box

---
+++ src/main/java/com/ovani4/javacore/chapter06/Box.java
@@ -0,0 +1,29 @@
+package main.java.com.ovani4.javacore.chapter06;
+
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+class BoxDemo{
+    public static void main(String[] args) {
+        Box myBox = new Box();
+        Box myBox2 = new Box();
+        double vol;
+
+        myBox.width = 10;
+        myBox.height = 20;
+        myBox.depth = 15;
+
+        myBox2.width = 2;
+        myBox2.height = 4;
+        myBox2.depth = 6;
+
+        vol = myBox.depth * myBox.height * myBox.width;
+        System.out.println("Объем равен " + vol);
+
+        vol = myBox2.depth * myBox2.height * myBox2.width;
+        System.out.println("Объем равен " + vol);
+
+    }
+}
