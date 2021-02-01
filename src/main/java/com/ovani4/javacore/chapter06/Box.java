54c6fd33983921429974b03fe76346e8b668913e
Ovani4 <ovani4@ya.ru>
2021-02-01 12:54:15 +0000

    create method void volume

--- src/main/java/com/ovani4/javacore/chapter06/Box.java
+++ src/main/java/com/ovani4/javacore/chapter06/Box.java
@@ -4,6 +4,11 @@ class Box {
     double width;
     double height;
     double depth;
+
+    void volume(){
+        System.out.print("Объем равен ");
+        System.out.println(width * height * depth);
+    }
 }
 class BoxDemo{
     public static void main(String[] args) {
@@ -19,11 +24,14 @@ class BoxDemo{
         myBox2.height = 4;
         myBox2.depth = 6;
 
-        vol = myBox.depth * myBox.height * myBox.width;
-        System.out.println("Объем равен " + vol);
+//        vol = myBox.depth * myBox.height * myBox.width;
+//        System.out.println("Объем равен " + vol);
+//
+//        vol = myBox2.depth * myBox2.height * myBox2.width;
+//        System.out.println("Объем равен " + vol);
 
-        vol = myBox2.depth * myBox2.height * myBox2.width;
-        System.out.println("Объем равен " + vol);
+        myBox.volume();
+        myBox2.volume();
 
     }
 }
